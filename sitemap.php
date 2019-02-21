2bc2ee24b68a829ce5204ee386e5ae3b40d19a86
matt smith <matt@matts-MacBook-Pro-2.local>
2019-02-21 08:46:02 +0000

    updated sitemap links too

--- sitemap.php
+++ sitemap.php
@@ -45,11 +45,25 @@
 </h1>
 
 <ul>
-    <li><a href="index.php">Home</a>
-    <li><a href="about.php">About us</a>
-    <li><a href="list.php">DVD ratings</a>
-    <li><a href="contact.php">Contact us</a>
-    <li><a href="sitemap.php">Site Map</a>
+    <li>
+        <a href="index.php">Home</a>
+    </li>
+
+    <li>
+        <a href="about.php">About Us</a>
+    </li>
+
+    <li>
+        <a href="list.php">Movie ratings</a>
+    </li>
+
+    <li>
+        <a href="contact.php">Contact Us</a>
+    </li>
+
+    <li>
+        <a href="sitemap.php">Site Map</a>
+    </li>
 </ul>
 
 
